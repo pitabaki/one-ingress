ec10877ae6a4dd1712991fdcc8f8432bfe51965a
Peter Berki <pberki1986@gmail.com>
2016-05-28 12:13:43 -0700

    Footer and Header made more mobile. Index floats fixed

--- development/js/scripty.js
+++ development/js/scripty.js
@@ -1,68 +1,109 @@
 (function(){
-	$('.dropdown-button').dropdown({
-		inDuration: 500,
-		outDuration: 225,
-		constrain_width: false, // Does not change width of dropdown to that of the activator
-		hover: false, // Activate on hover
-		gutter: 0, // Spacing from edge
-		belowOrigin: false, // Displays dropdown below the button
-		alignment: 'left' // Displays dropdown with edge aligned to the left of button
-	});
-	$('.dropdown-constant').hover(function(){
-		$('.nest-dropdown').css('cssText', 'min-height: 0px !important;');
-	});
-	$('.nest-trigger').hover(function(){
-		$('.nest-dropdown').css('cssText', 'min-height: 150px !important;');
-	});
-	$('#login').click(function(){
-		event.preventDefault();
-		$('#black-bg').css({
-			"display":"block"
-		});
-		setTimeout(function(){
-			$('#black-bg').css({
-				'opacity':'0.7'
-			});
-		}, 50);
-		setTimeout(function(){
-			$('#login-modal').css({
-				'display':'block',
-			});
-			setTimeout(function(){
-				$('#login-modal').css({
-					'bottom':'0%'
-				});
-			}, 50);
-		}, 50);
-		/*$('.modal').css({
-			'bottom':'0%'
-		});*/
-	});
-	$("#black-bg").click(function(){
-		$('#black-bg').css({
-			"opacity":"0"
-		});
-		setTimeout(function(){
-			$('#black-bg').css({
-				'display':'none'
-			});
-		}, 100);
-		setTimeout(function(){
-			$('#login-modal').css({
-				'bottom':'-100%',
-			});
-			setTimeout(function(){
-				$('#login-modal').css({
-					'display':'none'
-				});
-			}, 100);
-		}, 50);
-	});
-	$('#form01').submit(function(){
-		var name = document.getElementById('email-form01');
-		name = name.value;
-		$('#display-name').append("<p>Hi, " + name + "</p>");
-		return false
-	});
+    'use strict';
+    function bgFade(){
+        $('#black-bg').css({
+            "opacity":"0"
+        });
+        setTimeout(function(){
+            $('#black-bg').css({
+                'display':'none'
+            });
+        }, 100);
+        setTimeout(function(){
+            $('#login-modal').css({
+                'bottom':'-100%',
+            });
+            setTimeout(function(){
+                $('#login-modal').css({
+                    'display':'none'
+                });
+            }, 100);
+        }, 50);
+    }
+    console.log(document.getElementById('menu'));
+    $('#menu-icon').hover(function(){
+    	$('#menu-id').css({
+    		'left': '0'
+    	});
+    }, function(){
+        $('#menu-id').css({
+            'left': '-40px'
+        });
+    });
+    $('.dropdown-button').dropdown({
+        inDuration: 500,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: false, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: false, // Displays dropdown below the button
+        alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    });
+
+    $('.dropdown-constant').hover(function(){
+        $('.nest-dropdown').css('cssText', 'min-height: 0px !important;');
+    });
+
+    //Don't add "out" handler. It's important the user hovers over '.dropdown-constant' to close out '.nest-trigger'
+
+    $('.nest-trigger').hover(function(){
+        $('.nest-dropdown').css('cssText', 'min-height: 150px !important;');
+    });
+
+    //UX Implementation: For Mobile Users, click functionality is necessary
+    $('.nest-trigger').click(function(){
+        var nestDropdown = $('.nest-dropdown');
+        var height = nestDropdown.css('min-height');
+        if(height !== "0px"){
+            $('.nest-dropdown').css('cssText', 'min-height: 0px !important;');
+        }else{
+            $('.nest-dropdown').css('cssText', 'min-height: 150px !important;');
+        }
+        return false;
+    });
+    $('#login').click(function(){
+        $('#black-bg').css({
+            "display":"block"
+        });
+        setTimeout(function(){
+            $('#black-bg').css({
+                'opacity':'0.7'
+            });
+        }, 50);
+        setTimeout(function(){
+            $('#login-modal').css({
+                'display':'block',
+            });
+            setTimeout(function(){
+                $('#login-modal').css({
+                    'bottom':'0%'
+                });
+            }, 50);
+        }, 50);
+        return true;
+        /*$('.modal').css({
+            'bottom':'0%'
+        });*/
+    });
+    $("#black-bg").click(function(){
+    	bgFade();
+    });
+    $('#form01').submit(function(){
+        var name = document.getElementById('username-form01');
+        name = name.value;
+
+        var displayChillins = $('#display-name').children();
+        if(displayChillins.length === 0){
+        	$('#display-name').append("<p id='user-id'>Hi, " + name + "</p>");
+        	bgFade();
+        }else{
+        	$('#user-id').text("Hi, " + name);
+        	bgFade();
+        }
+        return false;
+    });
+    $(document).ready(function(){
+        $('.carousel').carousel({full_width: true});
+    });
 })
 ();
