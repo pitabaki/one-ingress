a53304db570428c9c0c2c387d89408aa226075b6
Peter Berki <pberki1986@gmail.com>
2016-06-07 00:49:25 -0700

    Sliders work for mobile users. Ready for user testing (desktop)

--- production/js/scripty.js
+++ production/js/scripty.js
@@ -95,9 +95,11 @@ function init(){
         alignment: 'left' // Displays dropdown with edge aligned to the left of button
     });
 
+    // Removed feature; poor UX
+    /*
     $('.dropdown-constant').hover(function(){
         $('.nest-dropdown').css('cssText', 'min-height: 0px !important;');
-    });
+    });*/
 
     //Don't add "out" handler. It's important the user hovers over '.dropdown-constant' to close out '.nest-trigger'
 
@@ -163,16 +165,15 @@ function init(){
 
     $('.down-arrow').click(function(){
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 80
-            }, 500);
-            return false;
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                  scrollTop: target.offset().top - 80
+                }, 500);
+                return false;
             }
-          }
+        }
     });
 
     /*************************************************
@@ -188,16 +189,27 @@ function init(){
                 def;
 
             $.each(data, function(key, val){
-                //console.log(val);
-                if (key == suiteType){
+                if ((key === suiteType) && (suiteType !== "def")){
                     suiteInfo.push("<img src='" + val.image + "' alt='" + val.name + "'><h2>" + val.title + "</h2><p>" + val.summary + "</p><div class='outer-center'><div class='mid-left'><a href='" + val.external + "'>Learn More </a></div><div class='mid-left icon'><a href='" + val.external + "'><img class='icon-arrow' src='img/icon_blue_arrow.png' alt='' /></a></div></div>");
-                }/*else if(key == "def"){
-                    def = val;
-                    //console.log(def);
-                    suiteInfo.push("<img src='" + def.image + "' alt='" + def.name + "'>");
-
-                }*/
+                }
             });
+            if(suiteInfo.length < 1){
+                $.each(data, function(key, val){
+                    if(key === "def"){
+                        def = val;
+                        suiteInfo.push("<div class='default-text'><h2>Oops! We are still working!</h2><p>We plan on launching this suite soon.<br>Be on the look out!</p></div><img src='" + def.image + "' alt='Page under construction'>");
+                    }
+                });
+                $( "<div/>", {
+                    "class": "col s12 m12 l12 center default-img",
+                    html: suiteInfo
+                }).appendTo( '.product-summaries' );
+                setTimeout(function(){
+                    $('.product-summaries').css({'max-height':'800px'});
+                    $('.default-text').css({'opacity':'1'});
+                }, 50);
+                return false;
+            }
             $( "<div/>", {
                 "class": "col s12 m12 l8 center",
                 html: suiteInfo
@@ -207,19 +219,35 @@ function init(){
             }, 50);
         });
     }
+
     $('.suite').click(function(){
         var suiteType = $(this).data('product');
-        if($('.product-summaries').children().length > 0){
-            $('.product-summaries').css({'max-height':'0'});
-            setTimeout(function(){
-                $('.product-summaries').empty();
-            }, 500);
-            setTimeout(function(){
-                suiteJSON(suiteType);
-            }, 500);
-        }else{
-            suiteJSON(suiteType);
+        $('.suite').removeClass('active');
+        $(this).addClass('active');
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 80
+                }, 500);
+            }
         }
+        setTimeout( function () {
+            //$(this).addClass('active');
+            if($('.product-summaries').children().length > 0){
+                $('.default-text').css({'opacity':'0'});
+                $('.product-summaries').css({'max-height':'0'});
+                setTimeout(function(){
+                    $('.product-summaries').empty();
+                }, 500);
+                setTimeout(function(){
+                    suiteJSON(suiteType);
+                }, 500);
+            }else{
+                suiteJSON(suiteType);
+            }
+        }, 500);
         return false;
     });
 
@@ -239,10 +267,8 @@ function init(){
             $.each(data, function( key, val){
                 if (key == bigwig){
                     bio.push("<span id='" + bigwig + "' class='about-inner-desc'><h6><strong>" + val.name + "</strong>, " + val.job + "</h6><p>" + val.bio + "</p></span>" );
-                    //console.log(val.name);
                 }
             });
-            console.log(anchor);
             $( "<span/>", {
                 "class": "about-desc",
                 html: bio
@@ -257,6 +283,234 @@ function init(){
         setTimeout(function(){
             selected.empty();
         }, 100);
+    });
+
+    /*************************************************
+    //////////////////////////////////////////////////
+
+    Functions to operate the Index pages's slider
+
+    //////////////////////////////////////////////////
+    *************************************************/
+
+    $('#xcard-slider').ready(function(){
+        console.log("load working");
+        var divOpen = "<div class='row copy'><div class='index-slider'><div class='assistant'></div><div class='text'>",
+            rowOpen = "<div class='row'><div class='col s12 m6 l6 left slider-text'>",
+            linkOpen = "<div class='outer-left'><div class='mid-left'>",
+            rowOpenTwo = "<div class='row copy index-slide-inner'>",
+            divOpenTwo = "<div class='col s12 m12 l6'><div class='index-slider'><div class='assistant'></div>",
+            identifier = "#xcard-slider";
+
+        $.getJSON("ajax/xcard.json", function ( data ) {
+            var sliders = [];
+            $.each(data, function(key, val){
+                console.log("working each");
+                if (val.summaryTwo.length > 0) {
+                    console.log("working if");
+                    sliders.push(divOpen + "<img src='" + val.image + "' alt='" + val.name + "'><h3>" + val.title + "</h3>"+ rowOpen + "<h6>" + val.summaryTitle + "</h6><p>" + val.summary + "</p>" + linkOpen + "<a href='" + val.external + "'>Learn More</a></div><div class='mid-right icon'><a href='" + val.external + "'><img class='icon-arrow' src='img/icon_blue_arrow.png' /></a></div></div></div><div class='col s12 m6 l6 left slider-text'><h6>" + val.summaryTwoTitle + "</h6><p>" + val.summaryTwo + "</p></div>");
+                }else{
+                    sliders.push(rowOpenTwo + divOpenTwo + "<img src='" + val.image + "' alt='" + val.name + "'></div></div>" + divOpenTwo + "<div id='full-width' class='text left'><h3>" + val.title + "</h3><h4>" + val.summaryTitle + "</h4><p>" + val.summary + "</p></div></div></div></div>");
+                }
+            });
+            if(sliders.length <= 2){
+                sliders.push(sliders[0],sliders[1]);
+            }
+            $.each(sliders, function(index){
+                $( "<div/>", {
+                    "class": "control",
+                    html: sliders[index]
+                }).appendTo( identifier );
+            });
+            var sliderArray = $(".slider-outer").children(),
+                total = 0;
+            $.each(sliderArray, function(index){
+                total += 1;
+                $(this).data("sliders", index);
+            });
+            var last = total - 1;
+            $.each(sliderArray, function(){
+                if($(this).data("sliders") === 0){
+                    $(this).css({
+                        "display":"block"
+                    });
+                } else if ($(this).data("sliders") === 1){
+                    $(this).css({
+                        "display":"block",
+                        "left":"1200px",
+                        "right":"0",
+                        "opacity":"0"
+                    }); 
+                } else if ($(this).data("sliders") === last){
+                    $(this).css({
+                        "display":"block",
+                        "left":"-1200px",
+                        "opacity":"0"
+                    }); 
+                }
+            });
+        });
+    });
+
+    /*************************************************
+    //////////////////////////////////////////////////
+
+    Functions to operate the membership pages's slider
+
+    //////////////////////////////////////////////////
+    *************************************************/
+
+    $('#membership-slider').ready(function(){
+        var constantImgBegin = "<div class='row img'><div class='membership-slider img'><div class='assistant'></div><img src='",
+            constantCopyBegin = "<div class='row copy'><div class='membership-slider'><div class='assistant'></div><div class='text'>",
+            identifier = "#membership-slider";
+
+        $.getJSON("ajax/membership.json", function ( data ) {
+            var sliders = [];
+            $.each(data, function(key, val){
+                sliders.push(constantImgBegin + val.image + "' alt='" + val.name + "'></div></div>" + constantCopyBegin + "<h6>" + val.title + "</h6><p>" + val.summary + "</p></div></div></div>");
+            });
+            $.each(sliders, function(index){
+                $( "<div/>", {
+                    "class": "control",
+                    html: sliders[index]
+                }).appendTo( identifier );
+            });
+            var sliderArray = $(".slider-outer").children(),
+                total = 0;
+            $.each(sliderArray, function(index){
+                total += 1;
+                $(this).data("sliders", index);
+            });
+            var last = total - 1;
+            $.each(sliderArray, function(){
+                if($(this).data("sliders") === 0){
+                    $(this).css({
+                        "display":"block"
+                    });
+                } else if ($(this).data("sliders") === 1){
+                    $(this).css({
+                        "display":"block",
+                        "left":"1200px",
+                        "right":"0",
+                        "opacity":"0"
+                    }); 
+                } else if ($(this).data("sliders") === last){
+                    $(this).css({
+                        "display":"block",
+                        "right":"0",
+                        "left":"-1200px",
+                        "opacity":"0"
+                    }); 
+                }
+            });
+        });
+    });
+
+    $("#left-click").click(function(){
+        var sliderArray = $(".slider-outer").children(),
+            total = 0;
+        $.each(sliderArray, function(index){
+            total += 1;
+        });
+        var last = total - 1;
+        $.each(sliderArray, function(){
+            var slide = $(this).data("sliders");
+            if(slide === 0){
+                $(this).css({
+                    "right": "0",
+                    "left":"-1200px",
+                    "opacity": "0"
+                });
+                $(this).css("display", function(){
+                    setTimeout(function(){
+                        return "none";
+                    }, 50);
+                });
+            } else if (slide === 1) {
+                $(this).css({
+                    "display":"block",
+                    "left":"0",
+                    "right": "0",
+                    "opacity": "1"
+                });
+            } else if (slide === last) {
+                $(this).css("display","none");
+            }
+        });
+        $.each(sliderArray, function(index){
+            var newSlider = $(this).data("sliders") - 1;
+            if($(this).data("sliders") === 0){
+                $(this).data("sliders", last);
+            } else {
+               $(this).data("sliders", newSlider); 
+            }
+            console.log($(this).data("sliders"));
+        });
+        $.each(sliderArray, function(){
+            if ($(this).data("sliders") === 1) {
+                $(this).css({
+                    "display":"block",
+                    "left":"1200px",
+                    "right":"0",
+                    "opacity":"0"
+                });
+            }
+        });
+        return false;
+    });
+
+    $("#right-click").click(function(){
+        var sliderArray = $(".slider-outer").children(),
+            total = 0;
+        $.each(sliderArray, function(index){
+            total += 1;
+        });
+        var last = total -1;
+        $.each(sliderArray, function(){
+            var slide = $(this).data("sliders");
+            if(slide === 0){
+                $(this).css({
+                    "left": "1200px",
+                    "right":"0",
+                    "opacity": "0"
+                });
+                $(this).css("display", function(){
+                    setTimeout(function(){
+                        return "none";
+                    }, 50);
+                });
+            } else if (slide === last) {
+                $(this).css({
+                    "display":"block",
+                    "left":"0",
+                    "right": "0",
+                    "opacity": "1"
+                });
+            } else if (slide === 1) {
+                $(this).css("display","none");
+            }
+        });
+        $.each(sliderArray, function(index){
+            var newSlider = $(this).data("sliders") + 1;
+            if($(this).data("sliders") === last){
+                $(this).data("sliders", 0);
+            } else {
+               $(this).data("sliders", newSlider); 
+            }
+            console.log($(this).data("sliders"));
+        });
+        $.each(sliderArray, function(){
+            if ($(this).data("sliders") === last) {
+                $(this).css({
+                    "display":"block",
+                    "right":"0",
+                    "left":"-1200px",
+                    "opacity":"0"
+                });
+            }
+        });
+        return false;
     });
 }
 window.onload = init;
